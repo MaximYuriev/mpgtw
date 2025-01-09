2ddbea187b37a78db4aaa21308326f2df52a93c8
Maxim <yrev2011@gmail.com>
2025-01-09 15:58:19 +0700

    Initial commit

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
