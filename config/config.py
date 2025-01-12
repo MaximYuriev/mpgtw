9f0fe660d1e114720788ed822416e3afb5cf802e
Александра <alexandra.pavlovna2003@gmail.com>
2025-01-12 19:19:49 +0700

    Add private and public keys path

--- config/config.py
+++ config/config.py
@@ -9,3 +9,6 @@ POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
+
+PRIVATE_KEY_PATH = os.environ.get("PRIVATE_KEY_PATH")
+PUBLIC_KEY_PATH = os.environ.get("PUBLIC_KEY_PATH")
