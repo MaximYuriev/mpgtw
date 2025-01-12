13481afa5ff410bcea175e172e015d16089f2372
Александра <alexandra.pavlovna2003@gmail.com>
2025-01-12 19:20:10 +0700

    Edit imports

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,17 @@ __all__ = (
     "POSTGRES_PORT",
     "POSTGRES_HOST",
     "POSTGRES_USER",
+    "PRIVATE_KEY_PATH",
+    "PUBLIC_KEY_PATH",
 )
 
-from .db import POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_HOST, POSTGRES_USER, db_url, async_session
+from .config import (
+    POSTGRES_DB,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_HOST,
+    POSTGRES_USER,
+    PRIVATE_KEY_PATH,
+    PUBLIC_KEY_PATH,
+)
+from .db import db_url, async_session
