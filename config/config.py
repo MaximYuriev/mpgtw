33f856edfa5d21051ce026cfec33cc4d26e81999
Maxim <yrev2011@gmail.com>
2025-01-14 17:02:23 +0700

    Add cookie key name. Update keys paths

--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 from dotenv import load_dotenv
 
@@ -10,5 +11,7 @@ POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 
-PRIVATE_KEY_PATH = os.environ.get("PRIVATE_KEY_PATH")
-PUBLIC_KEY_PATH = os.environ.get("PUBLIC_KEY_PATH")
+PRIVATE_KEY_PATH=pathlib.Path(os.environ.get("PRIVATE_KEY_PATH"))
+PUBLIC_KEY_PATH=pathlib.Path(os.environ.get("PUBLIC_KEY_PATH"))
+
+COOKIE_KEY_NAME = os.environ.get("COOKIE_KEY_NAME")
